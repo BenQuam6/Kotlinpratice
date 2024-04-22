d087782e196e8591ad1945d64e83d6f8cbca83af
BenQuam6 <bendquam#gmail.com>
2024-04-22 13:52:34 -0700

    inital commit

---
+++ app/src/main/java/com/example/firstgo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.firstgo
+
+import  androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.delay
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myButton)
+        val welcomeMessage = findViewById<TextView>(R.id.textView2)
+
+
+
+        btnClickMe.setOnClickListener{
+            btnClickMe.text = "HAHA you clicked me!!"
+            welcomeMessage.text = "Hello, Welcome to the Jungle"
+
+
+
+
+        }
+
+
+}}
